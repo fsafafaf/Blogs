f4c94ac8806ee8c05af45f921eeb0bce10625a8b
gaofeng <234226015@qq.com>
2021-05-28 00:19:23 +0800

    chore: diff(2)

--- code/virtualDom.js
+++ code/virtualDom.js
@@ -94,6 +94,7 @@ _.isElementNode = function (node) {
 // ])
 
 // 接下来给 Element 提供一个 render 函数，将 Element 对象渲染成真实的 DOM 节点。
+//  虚拟 DOM 类
 class Element {
   constructor(tagName, attrs, children) {
     // 如果只穿 tagName,children
@@ -135,19 +136,20 @@ function el(tagName, attrs, children) {
   return new Element(tagName, attrs, children);
 }
 
-var ul = el("ul", { id: "list" }, [
-  el("li", { class: "item" }, ["item1"]),
-  el("li", { class: "item" }, ["item2"]),
-  el("li", { class: "item" }, ["item3"]),
-]);
+// var ul = el("ul", { id: "list" }, [
+//   el("li", { class: "item" }, ["item1"]),
+//   el("li", { class: "item" }, ["item2"]),
+//   el("li", { class: "item" }, ["item3"]),
+// ]);
 
-let ulRoot = ul.render();
-document.body.appendChild(ulRoot);
+// let ulRoot = ul.render();
+// document.body.appendChild(ulRoot);
 
 // 三：实现 diff 算法
 // 这里我们做的就是实现一个 diff 算法进行虚拟节点 Element 的对比，并返回一个 patch 对象
 // 用来存储两个节点不同的地方。这也是整个 virtual dom 实现最核心的一步。而 diff 算法又包含了两个不一样的算法，一个 O(n),一个 O(max(m,n))
 
+// 1. 同层元素比较 O(n)
 // 同级比较会出现 4 种情况
 // - 整个元素不同，即元素被替换 replace
 // - 元素的 attrs 不同
@@ -167,10 +169,11 @@ function diff(oldTree, newTree) {
 }
 
 // a: 首先从最顶层的元素依次往下进行比较，直到最后一层元素结束，并把每个层级的差异存到 patch 对象种，即实现 walk 方法
+// 解析：对 DOM 树进行深度遍历，并用 index 对节点进行标识，每层节点如果有变更，则 push 进 patches 对象中，以 index 为 key，并且根据变化的不同，分为 4 种类型。
 
 // walk 遍历查找节点差异
 function walk(oldNode, newNode, index, patches) {
-  let currenPatch = [];
+  let currentPatch = [];
 
   // 如果 oldNode 被 remove 了
   if (newNode === null || newNode === undefined) {
@@ -178,7 +181,7 @@ function walk(oldNode, newNode, index, patches) {
   }
   // 比较文本之间的不同
   else if (_.isString(oldNode) && _.isString(newNode)) {
-    if (newNode !== oldNode) currenPatch.push({ type: TEXT, content: newNode });
+    if (newNode !== oldNode) currentPatch.push({ type: TEXT, content: newNode });
   }
   // 比较 attrs 的不同
   else if (oldNode.tagName === newNode.tagName && oldNode.key === newNode.key) {
@@ -195,18 +198,18 @@ function walk(oldNode, newNode, index, patches) {
     //   if (oldNode.attrs.hasOwnProperty(attrName)) attrsPatches[attrName] = newNode.attrs[attrName];
     // }
     //  不能使用 Objcet.keys，因为 key: undefined 也会被算成一个属性
-    // if (Object.keys(attrsPatches).length > 0) currenPatch.push({ type: ATTRS, content: attrsPatches });
+    // if (Object.keys(attrsPatches).length > 0) currentPatch.push({ type: ATTRS, content: attrsPatches });
     let attrsPatches = diffAttrs(oldNode, newNode);
-    if (attrsPatches) currenPatch.push({ type: ATTRS, content: attrsPatches });
+    if (attrsPatches) currentPatch.push({ type: ATTRS, content: attrsPatches });
     // 递归进行子节点的 diff 比较
     diffChildren(oldNode.children, newNode.children, index, patches);
   } else {
-    currenPatch.push({ type: REPLACE, node: newNode });
+    currentPatch.push({ type: REPLACE, node: newNode });
   }
 
   // 节点有 diff，则记录进 patches, 以节点遍历的 index 进行标识
-  if (currenPatch.length) {
-    patches[index] = currenPatch;
+  if (currentPatch.length) {
+    patches[index] = currentPatch;
   }
 }
 
@@ -266,13 +269,17 @@ let ul1 = el("ul", { id: "list" }, [
   el("li", { class: "item" }, ["Item 1"]),
   el("li", { class: "item" }, ["Item 2"]),
 ]);
-let ul2 = el("ul", { id: "list" }, [
+let ulRoot = ul1.render();
+document.body.appendChild(ulRoot);
+
+let ul2 = el("ul", { id: "list1" }, [
   el("li", { class: "item1" }, ["Item 4"]),
   el("li", { class: "item2" }, ["Item 5"]),
 ]);
 let patches = diff(ul1, ul2);
 console.log(patches);
 
+// 实现 patch 方法，对 dom 树进行 update 操作
 function patch(rootNode, patches) {
   let walker = { index: 0 };
   walkPatch(rootNode, walker, patches);
@@ -282,7 +289,7 @@ function walkPatch(node, walker, patches) {
   let currentPatches = patches[walker.index]; // 从 patches 中取出当前操作 patch
 
   const len = node.childNodes ? node.childNodes.length : 0;
-  for(let i = 0; i <len; i++) {  // 深度遍历子节点
+  for (let i = 0; i < len; i++) {  // 深度遍历子节点
     let child = node.childNodes[i];
     walker.index++;
     walkPatch(child, walker, patches);
@@ -295,19 +302,49 @@ function walkPatch(node, walker, patches) {
 
 function dealPatches(node, currentPatches) {
   currentPatches.forEach(currentPatch => {
-    switch (currenPatch.type) {
+    switch (currentPatch.type) {
       case REPLACE:
+        let newNode = (typeof currentPatch.node === 'string')
+          ? document.createTextNode(currentPatch.node)
+          : currentPatch.node.render();
+        node.parentNode.replaceChild(newNode, node);
         break;
       case REORDER:
         break;
       case ATTRS:
+        // content 为 attrs 变化值的集合
+        setAttrs(node, currentPatch.content);
         break;
       case TEXT:
+        // content 是一个 text
+        if (node.textContent) {
+          node.textContent = currentPatch.content;
+        } else {
+          node.nodeValue = currentPatch.content;
+        }
         break;
       default:
-        throw new Error('Unknwn patch type' + currenPatch.type);
+        throw new Error('Unknwn patch type' + currentPatch.type);
     }
   })
 }
 
-patch(ul1.render(), patches);
+function setAttrs(node, props) {
+  for (let key in props) {
+    if (props[key] === void 0) {
+      node.removeAttribute(key);
+    } else {
+      let value = props[key];
+      _.setAttr(node, key, value);
+    }
+  }
+}
+
+setTimeout(() => patch(ulRoot, patches), 2000)
+
+// 总结：
+//    1.先用 Element 模拟 DOM 节点，然后通过 render 方法将 Element 还原成真实的 DOM
+//    2.通过完成 diff 算法，比较新旧 Element 的不同，并记录到 patch 对象中
+//    3.完成 patch 方法，将 patch 对象解析，从而完成 DOM 的 update
+
+//  参考文章 https://juejin.cn/post/6844903815460700168#heading-6
